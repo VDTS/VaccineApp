1b0573e94cd632d756520cf23a716065fbd971db
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-02-13 07:31:41 +0430

    refactor DateToMessageTimeHint for unit tests

--- src/Core/Converters/DateToMessageTimeHint.cs
+++ src/Core/Converters/DateToMessageTimeHint.cs
@@ -1,4 +1,4 @@
-﻿using Core.Enums;
+﻿using Core.Utility;
 using System.Globalization;
 
 namespace Core.Converters;
@@ -7,7 +7,7 @@ public class DateToMessageTimeHint : IValueConverter
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         var age = (DateTime)value;
-        return Minutes(age);
+        return age.Minutes();
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -15,43 +15,5 @@ public class DateToMessageTimeHint : IValueConverter
         return (bool)value ? 1 : 0;
     }
 
-    public string Minutes(DateTime age)
-    {
-        var nowTime = DateTime.UtcNow;
-        if (age.Year == nowTime.Year &&
-            age.Month == nowTime.Month &&
-            age.Day == nowTime.Day &&
-            age.Hour == nowTime.Hour &&
-            age.Minute == nowTime.Minute &&
-            age.Minute != nowTime.Second)
-        {
-            return string.Concat(Math.Abs(age.Second - nowTime.Second), " secs");
-        }
-        else if (age.Year == nowTime.Year &&
-                age.Month == nowTime.Month &&
-                age.Day == nowTime.Day &&
-                age.Hour == nowTime.Hour &&
-                age.Minute != nowTime.Minute)
-        {
-            return string.Concat(Math.Abs(age.Minute - nowTime.Minute), " mins");
-        }
-        else if (age.Year == nowTime.Year &&
-                age.Month == nowTime.Month &&
-                age.Day == nowTime.Day &&
-                age.Hour != nowTime.Hour)
-        {
-            return string.Concat(age.Hour, ":", age.Minute);
-        }
-        else if (age.Year == nowTime.Year &&
-                age.Month == nowTime.Month &&
-                age.Day != nowTime.Day)
-        {
-            var a = (Months)0;
-            return string.Concat((Months)(age.Month - 1), " ", age.Day);
-        }
-        else
-        {
-            return string.Concat((Months)(age.Month - 1), " ", age.Day, " '", (age.Year % 100));
-        }
-    }
+   
 }
---
+++ src/Core/Utility/DateTimeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Core.Enums;
+
+namespace Core.Utility;
+
+public static class DateTimeExtensions
+{
+    public static string Minutes(this DateTime dateTime, DateTime nowTime = default)
+    {
+        if (nowTime == default) nowTime = DateTime.UtcNow;
+
+        if (dateTime.Year == nowTime.Year &&
+            dateTime.Month == nowTime.Month &&
+            dateTime.Day == nowTime.Day &&
+            dateTime.Hour == nowTime.Hour &&
+            dateTime.Minute == nowTime.Minute &&
+            dateTime.Minute != nowTime.Second)
+        {
+            return string.Concat(Math.Abs(dateTime.Second - nowTime.Second), " secs");
+        }
+        else if (dateTime.Year == nowTime.Year &&
+                dateTime.Month == nowTime.Month &&
+                dateTime.Day == nowTime.Day &&
+                dateTime.Hour == nowTime.Hour &&
+                dateTime.Minute != nowTime.Minute)
+        {
+            return string.Concat(Math.Abs(dateTime.Minute - nowTime.Minute), " mins");
+        }
+        else if (dateTime.Year == nowTime.Year &&
+                dateTime.Month == nowTime.Month &&
+                dateTime.Day == nowTime.Day &&
+                dateTime.Hour != nowTime.Hour)
+        {
+            return string.Concat(dateTime.Hour, ":", dateTime.Minute);
+        }
+        else if (dateTime.Year == nowTime.Year &&
+                dateTime.Month == nowTime.Month &&
+                dateTime.Day != nowTime.Day)
+        {
+            var a = (Months)0;
+            return string.Concat((Months)(dateTime.Month - 1), " ", dateTime.Day);
+        }
+        else
+        {
+            return string.Concat((Months)(dateTime.Month - 1), " ", dateTime.Day, " '", (dateTime.Year % 100));
+        }
+    }
+}
