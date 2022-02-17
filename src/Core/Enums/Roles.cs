e35cab8db54b8740f4bcd809b44b0638e8b4426b
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-02-17 12:40:58 +0430

    fixes MainPage null exception

---
+++ src/Core/Enums/Roles.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Enums;
+
+public static class RolesList
+{
+    public static List<string> Roles { get; set; } = new() { "Admin", "Supervisor", "Mobilizer", "Parent" };
+}
--- src/VaccineApp/App.xaml.cs
+++ src/VaccineApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using VaccineApp.Shells.Views;
+﻿using Core.Enums;
+using VaccineApp.Shells.Views;
 
 namespace VaccineApp;
 
@@ -6,23 +7,10 @@ public partial class App : Application
 {
 	public App()
 	{
-		InitializeComponent();
-
-		SelectShell();
-    }
-
-    private async void SelectShell()
-    {
-		var idToken = await SecureStorage.GetAsync("IdToken");
 		var role = Preferences.Get("Role", "AnonymousRole");
 
-		if (!string.IsNullOrEmpty(idToken))
-		{
-			Current.MainPage = new Appshell(role);
-		}
-		else
-		{
-			Current.MainPage = new Accessshell();
-		}
-	}
+		InitializeComponent();
+
+		MainPage = RolesList.Roles.Contains(role) ? new Appshell(role) : new Accessshell();
+    }
 }
