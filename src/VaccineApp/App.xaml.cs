f5082e805d3099b12eaaa5f82316c601b4c64459
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-02-19 14:46:30 +0430

    fixes: not picking claims

--- src/VaccineApp/App.xaml.cs
+++ src/VaccineApp/App.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using Core.Enums;
+using DAL.Persistence;
 using VaccineApp.Shells.Views;
 
 namespace VaccineApp;
 
 public partial class App : Application
 {
-	public App()
+	public App(UnitOfWork unitOfWork)
 	{
 		var role = Preferences.Get("Role", "AnonymousRole");
 
 		InitializeComponent();
+
+		unitOfWork.AddClaims(
+			Preferences.Get("ClusterId", "AnonymousCluster"),
+			Preferences.Get("TeamId", "AnonymousTeam"),
+			Preferences.Get("FamilyId", "AnonymousFamily"));
 
 		MainPage = RolesList.Roles.Contains(role) ? new Appshell(role) : new Accessshell();
     }
