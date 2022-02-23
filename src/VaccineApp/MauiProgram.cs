fecad07608293ab9491eeca71bbe59ff1ce2e91e
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-02-23 16:53:54 +0430

    migrates from p12 to p13, renames GridLayout to Grid, removes Microsoft.Extensions.Hosting, removes FileProviders

--- src/VaccineApp/MauiProgram.cs
+++ src/VaccineApp/MauiProgram.cs
@@ -5,7 +5,6 @@ using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
 using RealCache.Factory;
 using System.Reflection;
 using VaccineApp.AppConfigs;
@@ -24,20 +23,21 @@ public static class MauiProgram
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            })
-            .Host.
-            ConfigureAppConfiguration((app, config) =>
-            {
-                var assembly = typeof(App).GetTypeInfo().Assembly;
-                config.AddJsonFile(new EmbeddedFileProvider(assembly), "AppConfigs.SettingsDefaultsValues.json", optional: false, false);
-                config.AddJsonFile(new EmbeddedFileProvider(assembly), "AppConfigs.AppSettings.json", optional: false, true);
-                config.AddJsonFile(new EmbeddedFileProvider(assembly), "SecretFiles.AppSecrets.json", optional: false, true);
             });
 
-        builder.Services.AddBlazorWebView();
+        using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("VaccineApp.AppConfigs.SettingsDefaultsValues.json"))
+            builder.Configuration.AddJsonStream(resource);
+
+        using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("VaccineApp.AppConfigs.AppSettings.json"))
+            builder.Configuration.AddJsonStream(resource);
+
+        using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("VaccineApp.SecretFiles.AppSecrets.json"))
+            builder.Configuration.AddJsonStream(resource);
 
         builder.Services.Configure<AppSecrets>(builder.Configuration.GetSection("AppSecrets"));
         builder.Services.Configure<SettingsDefaultsValues>(builder.Configuration.GetSection("SettingsDefaultsValues"));
+
+        builder.Services.AddBlazorWebView();
         builder.Services.AddViewModels();
         builder.Services.AddDeviceSpecificFeatures();
 
