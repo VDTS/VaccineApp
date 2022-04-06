60974976411a796fc0c01b49cbc0c9549c138db2
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-04-06 12:02:34 +0430

    enables nullable in RealCache

--- src/RealCache/Persistence/Migrations/DbContext.cs
+++ src/RealCache/Persistence/Migrations/DbContext.cs
@@ -5,7 +5,7 @@ namespace RealCache.Persistence.Migrations;
 
 public class DbContext<T> where T : class
 {
-    private SQLiteConnection _database;
+    private SQLiteConnection? _database;
     public void CreateDB(string role, string user = "user")
     {
         if (_database == null)
@@ -17,16 +17,19 @@ public class DbContext<T> where T : class
 
     public void CreateTable()
     {
-        _database.CreateTable(typeof(T));
+        if(_database != null)
+            _database.CreateTable(typeof(T));
     }
 
     public void Insert(T data)
     {
-        _database.Insert(data);
+        if (_database != null)
+            _database.Insert(data);
     }
+
 
     public IEnumerable<PeriodModel> Get()
     {
-        return _database.Table<PeriodModel>().ToList();
+        return _database != null ? _database.Table<PeriodModel>().ToList() : Enumerable.Empty<PeriodModel>();
     }
 }
